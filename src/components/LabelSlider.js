3f30937d74863246af42af49726a91076822c20e
McDevon <jussi.enroos@gmail.com>
2019-07-29 21:10:01 +0300

    Add label slider

---
+++ src/components/LabelSlider.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Slider from 'react-input-slider'
+
+const LabelSlider = ({ label, value, step, min, max, onChange, suffix='' }) => {
+    const divStyle = {
+        marginTop: '5px',
+        marginLeft: '5px',
+        marginRight: '5px',
+        marginBottom: '5px'
+    }
+    return <div style={divStyle}>
+        <div>
+            {`${label}: ${value}${suffix}`}
+        </div>
+        <Slider
+            axis="x"
+            xstep={step}
+            xmin={min}
+            xmax={max}
+            x={value}
+            onChange={onChange()}
+        />
+    </div>
+}
+
+export default LabelSlider
